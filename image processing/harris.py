7b27512c2c1d604035e7f970ca6046584b34a7eb
Ray Bao <baoruihan@gmail.com>
2014-03-16 03:00:08 +0900

    ADD HARRIS CONER MATCHING

--- image processing/harris.py
+++ image processing/harris.py
@@ -7,8 +7,34 @@ Harrris corner
 from PIL import Image
 import numpy as np
 import imtools
+from pylab import *
+
+
 im = np.array(Image.open('lena.png').convert('L'))
 harrisim = imtools.compute_harris_response(im)
 filtered_coords = imtools.get_harris_points(harrisim,6,threshold = 0.05)
 imtools.plot_harris_points(im, filtered_coords)
+
+
+## matching
+figure()
+wid = 5
+im1 = np.array(Image.open('lena.png').convert('L'))
+im2 = np.array(Image.open('lena.png').convert('L'))
+
+
+harrisim = imtools.compute_harris_response(im1,5)
+filtered_coords1 = imtools.get_harris_points(harrisim,wid+1)
+d1 = imtools.get_descriptors(im1,filtered_coords1,wid)
+
+harrisim = imtools.compute_harris_response(im2,5)
+filtered_coords2 = imtools.get_harris_points(harrisim,wid+1)
+d2 = imtools.get_descriptors(im2,filtered_coords2,wid)
+
+print 'strarting matching'
+matches = imtools.match_twosided(d1,d2)
+
+gray()
+imtools.plot_matches(im1,im2,filtered_coords1, filtered_coords2, matches[:100])
+show()
 
--- image processing/imtools.py
+++ image processing/imtools.py
@@ -75,6 +75,9 @@ def denoise (im, U_init, tolerance=0.1, tau=0.125,tv_weight=100):
       
         return U, im-U
 
+##############################################################################
+#The following is used for Harris 
+##############################################################################
 from scipy.ndimage import filters
 
 def compute_harris_response(im,sigma = 3):
@@ -140,7 +143,92 @@ def plot_harris_points(image,filtered_coords):
     axis('off')
     show()
 
+def get_descriptors(image, filtered_coords, wid=5):
+    """ For each point return pixel values around the point
+    using a neibourhood of width 2*wid+1
+    """
+    desc = []
+    for coords in filtered_coords:
+        patch = image[coords[0]-wid:coords[0]+wid+1,
+                      coords[1]-wid:coords[1]+wid+1].flatten()
+        desc.append(patch)
+        
+    return desc
+    
+def match(desc1, desc2, threshold=0.5):
+    """ For each corner point descriptor in the first image,
+    select its match to second image using 
+    normalized cross correlation."""
+    
+    n = len(desc1[0])
+    #pair-wise distance
+    d = -ones((len(desc1),len(desc2)))
+    for i in range(len(desc1)):
+        for j in range(len(desc2)):
+            d1 = (desc1[i]-mean(desc1[i]))/std(desc1[i])
+            d2 = (desc2[j]-mean(desc2[j]))/std(desc2[j])
+            ncc_value = sum(d1*d2)/(n-1)
+            if ncc_value > threshold:
+                d[i,j]= ncc_value
+            
+    ndx = argsort(-d)
+    matchscores = ndx[:,0]
+    
+    return matchscores
+    
+ 
+def match_twosided(desc1,desc2,threshold=0.5):
+    """ Two-sided symmetric version of match()."""
+    matches_12 = match(desc1,desc2,threshold)
+    matches_21 = match(desc2,desc1,threshold)
+    ndx_12 = where(matches_12>=0)[0]
+    
+    # remove matches that are not symmetric
+    for n in ndx_12:
+        if matches_21[matches_12[n]] !=n:
+            matches_12[n] = -1
+            
+    return matches_12
+    
+    
+def appendimages(im1,im2):
+    """"Return a new image that appeds the two images side by side."""
+    
+    # select the image with the fewest rows and fill in enough empty rows
+    rows1 = im1.shape[0]
+    rows2 = im2.shape[0]
+    
+    if rows1 <rows2:
+        im1 = concatenate((im1,zeros((rows2-rows1,im1.shape[1]))), axis = 0)
+    elif rows1 >rows2:
+        im2 = concatenate((im2,zeros((rows1-rows2,im2.shape[1]))), axis = 0)
+        
+    return concatenate((im1,im2),axis = 1)
 
+def plot_matches(im1,im2,locs1,locs2,matchscores,show_below=True)   :
+    """ Show a figure with lines joining the accepted matches
+    input: im1, im2(image as arrays), locs1, locs2(feature location)
+    """
+    
+    im3 = appendimages(im1, im2)
+    imshow(im3)
+    
+    cols1 = im1.shape[1]
+    for i,m in enumerate(matchscores):
+        if m>0:
+            plot([locs1[i][1],locs2[m][1]+cols1],[locs1[i][0],locs2[m][0]],'c')
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
