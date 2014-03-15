8e3984cc1c3bd9836cdb2acbb636f35c43b851af
Ray Bao <baoruihan@gmail.com>
2014-03-15 15:22:43 +0900

    local commit

--- image processing/imdenoise.py
+++ image processing/imdenoise.py
@@ -2,15 +2,16 @@ from numpy import *
 from numpy import random
 from scipy.ndimage import filters
 import imtools
+from PIL import Image
 
-#im = zeros((500,500))
-#im[100:400,100:400] = 128
-#im[200:300,200:300] = 255
+im = zeros((500,500))
+im[100:400,100:400] = 128
+im[200:300,200:300] = 255
 
-#im = im+ 30*random.standard_normal((500,500))
-im = array(Image.open('DSCF7481.JPG').convert('L'))
+im = im+ 30*random.standard_normal((500,500))
+#im = array(Image.open('DSCF7481.JPG').convert('L'))
 U,T = imtools.denoise(im,im)
-#G= filters.gaussian_filter(im,10)
+G= filters.gaussian_filter(im,10)
 
 import scipy.misc
 scipy.misc.imsave('synth_rof.pdf',U)
