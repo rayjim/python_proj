0682a834958a2def70452828b4ac59bedb74968d
bao <r-bao@ay.jp.nec.com>
2014-04-01 15:10:59 +0900

    add warp function

--- image_processing/warp.py
+++ image_processing/warp.py
@@ -90,3 +90,51 @@ def plot_mesh(x,y,tri):
         plot(x[t_ext],y[t_ext],'r')
         
 #paranoma
+def panorama(H,fromim,toim,padding=2400,delta=2400):
+    """Create horizontal panorama by blending two images
+    using a homography H (preferably estimated using RANSAC)
+    the result is an image with the same height as to im,'padding'
+    specifies the number of fill pixels and 'delta'adding additional translation."""
+    # check if images are gray scale or color
+    is_color = len(fromim.shape)=3
+    #homography translation for geometric_transform()
+    def transf(p):
+        p2 = np.dot(H,[p[0],p[1],1])
+        return (p2[0]/p2[2],p2[1]/p2[2])
+    if H[1,2]<0:
+        print 'warp-right'
+        if is_color:
+            toim_t = np.hstack((toim,np.zeros((toim.shape[0],padding,3))))
+            fromim_t = np.zeros((toim.shape[0],toim.shape[1]+padding,toim.shape[2]))
+            for col in range(3):
+                fromim_t[:,:,col] = ndimage.geometric_transform(fromim[:,:,col],
+                trasf,(toim.shape[0],toim.shape[1]+padding))
+            else:
+                toim_t = np.hstack((toim,np.zeros((toim.shape[0],padding))))
+                fromim_t = ndimage.geometric_transform(fromim,transf,(toim.shape[0],
+                            toim.shape[1]+padding))
+    else:
+         print 'warp - left'
+         # add translation to compensate for padding to the left
+         H_delta = np.array([[1,0,0],[0,1,-delta],[0,0,1]])
+         H = np.dot(H,H_delta)
+         #transform from im
+         if is_color:
+             toim_t = np.hstack((np.zeros((toim.shape[0],padding,3)),toim))
+             fromim_t = np.zeros((toim.shape[0],toim.shape[1]+padding,toim.shape[2]))
+             for col in range(3):
+                 fromim_t[:,:,col]= ndimage.geometric_transform(fromim[:,:,col],
+                                    transf,(toim.shape[0],toim.shape[1]+padding))
+         else:
+             toim_t = np.hstack((np.zeros((toim.shape[0],padding)),toim))
+             fromim_t = ndimage.geometric_transform(fromim,transf,(toim.shape[0],toim.shape[1]+padding))
+    #blend and return(put from im above toim)
+    if is_color:
+        #all non black pixels
+        alpha = ((fromim_t[:,:,0]*fromim_t[:,:,1]*fromim_t[:,:,2])>0)
+        for col in range(3):
+            toim_t[:,:,col]=fromim_t[:,:,col]*alpha+toim_t[:,:,col]*(1-alpha)
+    else:
+        alpha = (fromim_t>0)
+        toim_t = fromim_t*alpha+toim_t*(1-alpha)
+    return toim_t
