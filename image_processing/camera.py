bc14ecb968bf1f87ac90d10dd43f97f6d314e192
bao <baoruihan@gmail.com>
2014-04-03 17:27:55 +0900

    add factor feature for camera

--- image_processing/camera.py
+++ image_processing/camera.py
@@ -27,3 +27,18 @@ def rotation_matrix(a):
     R = np.eye(4)
     R[:3,:3]= linalg.expm(np.array([[0,-a[2],a[1]],[a[2],0,-a[0]],[-a[1],a[0],0]]))
     return R
+    
+def factor(self):
+    """Factorize the camera matrix into K, R, t as P=K[P|t]"""
+    K,R = linalg.rq(self.P[:,:3])
+    #make diagonal of K positive
+    T = np.diag(np.sign(np.diag(K)))
+    if linalg.det(T)<0:
+        T[1,1]*=-1
+        
+    self.K = np.dot(K,T)
+    self.R = np.dot(T,R)
+    self.t = np.dot(linalg.inv(self.K),self.P[:,3])
+    
+    return self.K, self.R, self.t
+    
