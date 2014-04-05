88be4c651915931408cf9d801e684297a96eef8b
Ray Bao <baoruihan@gmail.com>
2014-04-05 11:35:10 +0900

    implemented camera calibration and homography estimation

--- image_processing/camera.py
+++ image_processing/camera.py
@@ -52,13 +52,4 @@ def rotation_matrix(a):
     return R
     
 
-#compute calibration matrix
-def my_calibration(sz):
-    row,col = sz
-    fx = 2555*col/2592
-    fy = 2586*row/1936
-    K = np.diag([fx,fy,1])
-    K[0,2] = 0.5*col
-    K[1,2] = 0.5*row
-    return K
-    
+
