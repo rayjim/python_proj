eaa27c02ed5cb6cfbde4b45d5a0fde4cd112bf18
bao <baoruihan@gmail.com>
2014-04-11 17:13:23 +0900

    LK

--- image_processing/sfm/ex_LK.py
+++ image_processing/sfm/ex_LK.py
@@ -12,20 +12,22 @@ from scipy import ndimage
 from scipy.linalg import block_diag
 
 imname = "../lena.png"
-im1 = np.array(Image.open(imname).convert('L'))
-im2 = np.array(Image.open(imname).convert('L'))
+im0 = np.array(Image.open(imname).convert('L')).astype('float')
+P_true = np.array([[1,0,-5],[0,1,-8]])
+im1 = ndimage.affine_transform(im0,P_true[:2,:2],(P_true[0,2],P_true[1,2]))
+im2 = np.array(Image.open(imname).convert('L')).astype('float')
 #im1_gradx = ndimage.sobel(im1,axis=0,mode='constant')
-#im1_gradx, im1_grady = np.gradient(im1)
+im1_gradx, im1_grady = np.gradient(im1)
 #im1_grady = ndimage.sobel(im1,axis=1,mode='constant')
 mx=array([[-1,0,1],[-1,0,1],[-1,0,1]])
 my=array([[-1,-1,-1],[0,0,0],[1,1,1]])
-im1_gradx = ndimage.convolve(im1,mx)
-im1_grady = ndimage.convolve(im1,my)
+#im1_gradx = ndimage.convolve(im1,mx)
+#im1_grady = ndimage.convolve(im1,my)
 gray()
 MAXITERS = 1000
-P = np.ones((2,3))*.5
-#P=np.zeros((2,3))
-
+#P = np.ones((1,6))
+P=np.ones((2,3))
+#P = np.array([[1.1,0.1,1],[0.1,0.9,1]])
 def p_jacob(x,y):
     return np.array([[x,0,y,0,1,0],[0,x,0,y,0,1]])
 #generate jacobian
@@ -37,36 +39,53 @@ def p_jacob(x,y):
 #    
 e = 10e-5
 delta = 500
-
+def imshow_grad(im):
+    """show gradient image by change the dynamic ranges"""
+    imshow((im+255)/2)
 
 for iter in range(MAXITERS):
     # form image 
     
-    if (np.linalg.norm(delta)<10e-5):
+    if (np.linalg.norm(delta)<10e-3):
         print "over at ",iter,"iterations"
         
         break;
     #step1: Warp image1
     im_W = ndimage.affine_transform(im1,P[:2,:2],(P[0,2],P[1,2]))
+    #print np.linalg.norm(im_W)
     #step2: compute the error image
-    im_E = im2 - im_W
+    im_E = im2 - im_W.astype('int')
     #step3: Warp the gradiant
     imx_W = ndimage.affine_transform(im1_gradx,P[:2,:2],(P[0,2],P[1,2]))
     imy_W = ndimage.affine_transform(im1_grady,P[:2,:2],(P[0,2],P[1,2]))
     #im_W = np.vstack((imx_W.flatten(),imy_W.flatten()))
     res = []
+    test = []
     #step 4 and 5
     for i in range(im1.shape[0]):
         for j in range(im1.shape[1]):
             res.append(np.dot(np.array([imx_W[i,j],imy_W[i,j]]),p_jacob(i,j)))
-    grad = np.array(res)
+            test.append((i,j))
+    sd = np.array(res)
+    ta = np.array(test)
     #hessian
     #step6
-    H = np.dot(grad.T,grad)
+    H = np.dot(sd.T,sd)
     #step7
-    d = np.dot(grad.T,(im_E).flatten())
+    d = np.dot(sd.T,(im_E).flatten())
     #step 8
     delta = np.linalg.solve(H,d)
-    print np.linalg.norm(delta)
-    P = (P.flatten()+delta).reshape(2,3)
+    print np.linalg.norm(im_E)
+    P = (P.T.flatten()+delta).reshape(3,2).T
+
+
 imshow(im_E)
+
+
+#test code
+res = []
+for i in range(im1.shape[0]):
+        for j in range(im1.shape[1]):
+            res.append(np.dot(np.array([im1_gradx[i,j],im1_grady[i,j]]),p_jacob(i,j)))
+        
+aa = np.array(res)
