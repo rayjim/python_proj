f6bfb5f82da67154477424570defab3c88aec0ae
Ray Bao <baoruihan@gmail.com>
2014-04-13 17:43:08 +0900

    work case

--- image_processing/sfm/ex_LK.py
+++ image_processing/sfm/ex_LK.py
@@ -13,9 +13,10 @@ from scipy.linalg import block_diag
 
 imname = "../lena.png"
 im0 = np.array(Image.open(imname).convert('L')).astype('float')
-P_true = np.array([[1,0,-5],[0,1,-8]])
+P_true = np.array([[1,0,5],[0,1,8]])
 im1 = ndimage.affine_transform(im0,P_true[:2,:2],(P_true[0,2],P_true[1,2]))
 im2 = np.array(Image.open(imname).convert('L')).astype('float')
+#im2 = im1
 #im1_gradx = ndimage.sobel(im1,axis=0,mode='constant')
 im1_gradx, im1_grady = np.gradient(im1)
 #im1_grady = ndimage.sobel(im1,axis=1,mode='constant')
@@ -27,7 +28,7 @@ gray()
 MAXITERS = 1000
 #P = np.ones((1,6))
 P=np.ones((2,3))
-#P = np.array([[1.1,0.1,1],[0.1,0.9,1]])
+P = np.array([[1.1,0.1,1],[0.1,0.9,1]])
 def p_jacob(x,y):
     return np.array([[x,0,y,0,1,0],[0,x,0,y,0,1]])
 #generate jacobian
@@ -46,7 +47,7 @@ def imshow_grad(im):
 for iter in range(MAXITERS):
     # form image 
     
-    if (np.linalg.norm(delta)<10e-3):
+    if (np.linalg.norm(delta)<10e-4):
         print "over at ",iter,"iterations"
         
         break;
@@ -54,7 +55,7 @@ for iter in range(MAXITERS):
     im_W = ndimage.affine_transform(im1,P[:2,:2],(P[0,2],P[1,2]))
     #print np.linalg.norm(im_W)
     #step2: compute the error image
-    im_E = im2 - im_W.astype('int')
+    im_E = im2 - im_W.astype('float')
     #step3: Warp the gradiant
     imx_W = ndimage.affine_transform(im1_gradx,P[:2,:2],(P[0,2],P[1,2]))
     imy_W = ndimage.affine_transform(im1_grady,P[:2,:2],(P[0,2],P[1,2]))
@@ -75,7 +76,8 @@ for iter in range(MAXITERS):
     d = np.dot(sd.T,(im_E).flatten())
     #step 8
     delta = np.linalg.solve(H,d)
-    print np.linalg.norm(im_E)
+    print np.linalg.norm(delta)
+    print P
     P = (P.T.flatten()+delta).reshape(3,2).T
 
 
